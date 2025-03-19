77af738bca9409e69338123e24a12f8c81bd57d4
Jocy Gonçalves Neto <jocygneto@icloud.com>
2025-03-19 15:38:43 -0300

    add base project

---
+++ PersonWebApplicaton/PersonWebApplicaton/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonWebApplicaton.Model;
+
+public class Person
+{
+    public long Id { get; set; }
+    public string firstName { get; set; }
+    public int lastNaem { get; set; }
+    public DateOnly birthDate { get; set; }
+    public string address { get; set; }
+    public string gender { get; set; }
+}
---
+++ PersonWebApplicaton/PersonWebApplicaton/Services/Implementation/PersonServiceImplementation.cs
@@ -0,0 +1,31 @@
+﻿using PersonWebApplicaton.Model;
+
+namespace PersonWebApplicaton.Services.Implemenatation;
+
+public class PersonServiceImplementation : IPersonService
+{
+    public Person Create(Person person)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Person> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Person GetById(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Person Update(Person person)
+    {
+        throw new NotImplementedException();
+    }
+}
