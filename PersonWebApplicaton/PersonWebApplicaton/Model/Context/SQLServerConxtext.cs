ceaf2bd5272b7e2236a54f03c19ac2300c3e2e73
Jocy Goncalves Neto <jocy.neto@portaldecompraspublicas.com.br>
2025-03-31 14:14:03 -0300

    Add SQL Server Context

---
+++ PersonWebApplicaton/PersonWebApplicaton/Model/Context/SQLServerConxtext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PersonWebApplicaton.Model.Context;
+
+public class SQLServerConxtext : DbContext
+{
+    public SQLServerConxtext() {}
+    public SQLServerConxtext(DbContextOptions<SQLServerConxtext> options) : base(options) { }
+
+    // DbSet é similar à uma list
+    public DbSet<Person> Persons { get; set; }
+}
